84fbddca3d0082432eb2c99e59321e8f100cc894
Aditya <83640028+Aditya0150@users.noreply.github.com>
2025-06-07 10:48:47 +0530

    Update Tour.jsx

--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -3,15 +3,15 @@ import { useNavigate } from "react-router-dom";
 import "../styles/Tour.css";
 
 const tours = [
+   { name: "Dronagiri Base Camp Trek", altitude: "4,200 m", image: "Dronagiri.JPG", description: "A remote and scenic trek to the base of Dronagiri peak, full of alpine beauty and legends." },
+  { name: "Satopanth", altitude: "4,600 m", image: "Satopanth.jpeg", description: "A high-altitude spiritual trek to the sacred Satopanth Lake near Badrinath." },
+  { name: "Kagbhusandi", altitude: "4,600 m", image: "Kagbhusandi.jpg", description: "A less-traveled mystical trek offering stunning alpine lakes and birdwatching opportunities." }
   { name: "Kedarnath", altitude: "3,583 m", image: "Kedarnath-Yatra.jpg", description: "A beautiful trek through the Himalayan ranges." },
   { name: "Valley of Flowers", altitude: "3,658 m", image: "Valley of Flower.png", description: "A trek amidst a valley full of blooming flowers." },
   { name: "Har Ki Dun", altitude: "3,566 m", image: "Har Ki Dun.png", description: "Explore the scenic Valley of Gods." },
   { name: "Kuari Pass", altitude: "4,265 m", image: "Kuari Pass.png", description: "A short trek with stunning panoramic views." },
   { name: "Kedarkantha", altitude: "3,800 m", image: "Kedarkantha.jpg", description: "A winter trek perfect for beginners." },
   { name: "Roopkund", altitude: "3,892 m", image: "Roopkund.jpg", description: "A high-altitude Himalayan trek famous for its glacial lake and ancient human skeletons.." },
-  { name: "Dronagiri Base Camp Trek", altitude: "4,200 m", image: "Dronagiri.JPG", description: "A remote and scenic trek to the base of Dronagiri peak, full of alpine beauty and legends." },
-  { name: "Satopanth", altitude: "4,600 m", image: "Satopanth.jpeg", description: "A high-altitude spiritual trek to the sacred Satopanth Lake near Badrinath." },
-  { name: "Kagbhusandi", altitude: "4,600 m", image: "Kagbhusandi.jpg", description: "A less-traveled mystical trek offering stunning alpine lakes and birdwatching opportunities." }
 ];
 
 const Tours = () => {
